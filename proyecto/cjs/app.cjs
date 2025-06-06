c0f07d7b1b801562fd2438d6ab3fa9a711b34bfe
Santiago Ayala <santitomayala07@gmail.com>
2025-06-06 11:10:31 -0300

    Se hizo la exportacion e importación de las funciones nombradas sumar y restar

---
+++ proyecto/cjs/app.cjs
@@ -0,0 +1,5 @@
+const modulo = require('./funcionesNombradas.cjs');
+
+console.log(modulo.suma(2,2));
+
+console.log(modulo.resta(6,2));
---
+++ proyecto/cjs/funcionesPorDefecto.cjs
@@ -0,0 +1,7 @@
+const multiplicar = (num1,num2) => {
+    return num1 * num2;
+}
+
+const dividir = (num1,num2) => {
+    return num1/num2;
+}
