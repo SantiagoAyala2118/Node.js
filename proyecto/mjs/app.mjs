84a0e5ce82f758e47a7fbe8e40cc090d4f8e53f4
Santiago Ayala <santitomayala07@gmail.com>
2025-06-06 11:28:21 -0300

    Se agegaron las exportaciones e importaciones por defecto de mjs

---
+++ proyecto/mjs/app.mjs
@@ -0,0 +1,12 @@
+//--------------Nombradas-----------------------------//
+import { sumar,restar } from './funcionesNombradas.mjs';
+
+console.log(sumar(2,3));
+console.log(restar(4,2));
+//-----------------------------------------------------//
+
+//------------------Por Defecto-----------------------//
+import funciones from './funcionesPorDefecto.mjs';
+
+console.log(funciones.multiplicar(5,2));
+console.log(funciones.dividir(10,2));
---
+++ proyecto/mjs/funcionesPorDefecto.mjs
@@ -0,0 +1,15 @@
+let funciones = {};
+
+
+
+funciones.multiplicar = (num1,num2)=> {
+    return  num1*num2;
+}
+
+funciones.dividir =(num1,num2) => {
+    return num1/num2;
+}
+
+export default funciones;
+
+
